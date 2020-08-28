658a5e04b12464726d596e1410878a85ceb2bcc9
andygubser <andy.gubser@pm.me>
2020-08-28 20:15:48 +0200

    preprocessor weather added

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,8 +6,10 @@ from preprocessor.paths import (PATH_TO_CONSUMER_1,
                                 PATH_TO_CONSUMER_6,
                                 PATH_TO_CONSUMER_7,
                                 PATH_TO_CONSUMER_8,
-                                PATH_TO_CONSUMER_9)
+                                PATH_TO_CONSUMER_9,
+                                PATH_TO_WEATHER)
 from preprocessor.preprocessor_customer import CustomerPreprocessor
+from preprocessor.preprocessor_weather import WeatherPreprocessor
 import matplotlib.pyplot as plt
 
 
@@ -21,5 +23,7 @@ customer1_in_hours = customer1.resample("d").mean()
 customer1_in_hours.plot(y="leistung_kw")
 plt.show()
 
+weather_preprocessor = WeatherPreprocessor(path_to_weather=PATH_TO_WEATHER)
+weather = weather_preprocessor.data_prepared
 
-customer2["leistung_kw"].idxmax()
+# customer2["leistung_kw"].idxmax()
--- preprocessor/preprocessor_weather.py
+++ preprocessor/preprocessor_weather.py
@@ -6,67 +6,24 @@ class WeatherPreprocessor:
         output: preprocessed dataframe with datetimeindex """
 
     def __init__(self, path_to_weather):
-        self.df_raw = pd.read_csv(path_to_weather)
-        df_renamed_columns = self._rename_columns(self.df_raw)
-        self.data_prepared = self._set_datetimeindex(df_renamed_columns)
+        self.df_raw = pd.read_csv(path_to_weather, encoding='latin1', delimiter=";", header=1, na_values="?")
+        df_dropped_rows = self.df_raw.drop(0)
+        df_renamed_columns = self._rename_columns(df_dropped_rows)
+        df_datetimeindexed = self._set_datetimeindex(df_renamed_columns)
+        self.data_prepared = df_datetimeindexed.dropna(how="all")
 
     @classmethod
     def _rename_columns(cls, df):
-        columns_keys = ['Timestamp',
-                        'T6 Aussentemp.(°C)',
-                        'T7 RL Primär(°C)',
-                        'T8 VL Sekundär(°C)',
-                        'T11 RL Sekundär(°C)',
-                        'Solltemp. VL Sekundär(°C)',
-                        'Max.Rücklauftemp.primär(°C)',
-                        'B-Solltemp.(°C)',
-                        'B-Status Pumpe',
-                        'B-Status Kreis',
-                        'B-Status Mischer',
-                        'Puffer EIN Oben(°C)',
-                        'Puffer AUS Unten(°C)',
-                        'Ventilstellung(%)',
-                        'AT Mittel(°C)',
-                        'AT Langzeit(°C)',
-                        'WZ Wärmemenge(kWh)',
-                        'WZ Leistung(kW)',
-                        'WZ Duchfluss(l/h)',
-                        'WZ Rücklauftemp.(°C)',
-                        'WZ Vorlauftemp.(°C)',
-                        'WZ Fehleranzeige',
-                        'WZ Spreizung(°C)',
-                        'Ventilstellung Gesamt(%)']
-
-        columns_values = ["timestamp",
-                          "aussentemperatur",
-                          "primär_temperatur_rl",
-                          "sekundär_temperatur_vl",
-                          "sekundär_temperatur_rl",
-                          "sekundär_temperatur_vl_soll",
-                          "maximale_rücklauftemperature",
-                          "temperatur_soll",
-                          "status_pump",
-                          "status_kreis",
-                          "status_mischer",
-                          "puffer_oben_ein",
-                          "puffer_unten_aus",
-                          "ventilstellung",
-                          "aussentemperatur_mittel",
-                          "aussentemperatur_langzeit",
-                          "wärmemenge_kwh",
-                          "leistung_kw",
-                          "durchfluss_liter_per_hour",
-                          "rücklauftemperatur",
-                          "vorlauftemperatur",
-                          "fehleranzeige",
-                          "spreizung_celsius",
-                          "ventilstellung_gesamt"]
-
-        columns_dict = dict(zip(columns_keys, columns_values))
-
-        return df.rename(columns_dict, axis=1)
+        # Parameter	Temperatur mittel	Rel. Feuchte mittel	Niederschlag	Windgeschwindigkeit mittel	Globalstrahlung
+        column_names = ["timestamp",
+                        "temperature",
+                        "relative_feuchtigkeit",
+                        "niederschlag",
+                        "windgeschwindigkeit",
+                        "globalstrahlung"]
+        df.columns = column_names
+        return df
 
     @classmethod
     def _set_datetimeindex(cls, df):
         return df.set_index("timestamp")
-
