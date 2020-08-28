e7be176afabe6b27500f864812b0bae4c6781dfd
andygubser <andy.gubser@pm.me>
2020-08-28 18:03:49 +0200

    preprocessor added

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,7 +1,17 @@
-import pandas as pd
-import os, sys
-from preprocessor.paths import (PATH_TO_CONSUMER_1)
+from preprocessor.paths import (PATH_TO_CONSUMER_1,
+                                PATH_TO_CONSUMER_2,
+                                PATH_TO_CONSUMER_3,
+                                PATH_TO_CONSUMER_4,
+                                PATH_TO_CONSUMER_5,
+                                PATH_TO_CONSUMER_6,
+                                PATH_TO_CONSUMER_7,
+                                PATH_TO_CONSUMER_8,
+                                PATH_TO_CONSUMER_9)
 from preprocessor.preprocessor_customer import CustomerPreprocessor
 
+
 customer1_preprocessor = CustomerPreprocessor(path_to_data=PATH_TO_CONSUMER_1)
-customer1_preprocessor = customer1_preprocessor.data_prepared
+customer1 = customer1_preprocessor.data_prepared
+
+customer2_preprocessor = CustomerPreprocessor(path_to_data=PATH_TO_CONSUMER_2)
+customer2 = customer2_preprocessor.data_prepared
--- preprocessor/preprocessor_customer.py
+++ preprocessor/preprocessor_customer.py
@@ -12,27 +12,58 @@ class CustomerPreprocessor:
 
     @classmethod
     def _rename_columns(cls, df):
-        columns_keys = ['Timestamp', 'T6 Aussentemp.(°C)', 'T7 RL Primär(°C)',
-                        'T8 VL Sekundär(°C)', 'T11 RL Sekundär(°C)',
-                        'Solltemp. VL Sekundär(°C)', 'Max.Rücklauftemp.primär(°C)',
-                        'B-Solltemp.(°C)', 'B-Status Pumpe', 'B-Status Kreis',
-                        'B-Status Mischer', 'Puffer EIN Oben(°C)', 'Puffer AUS Unten(°C)',
-                        'Ventilstellung(%)', 'AT Mittel(°C)', 'AT Langzeit(°C)',
-                        'WZ Wärmemenge(kWh)', 'WZ Leistung(kW)', 'WZ Duchfluss(l/h)',
-                        'WZ Rücklauftemp.(°C)', 'WZ Vorlauftemp.(°C)', 'WZ Spreizung(°C)',
+        columns_keys = ['Timestamp',
+                        'T6 Aussentemp.(°C)',
+                        'T7 RL Primär(°C)',
+                        'T8 VL Sekundär(°C)',
+                        'T11 RL Sekundär(°C)',
+                        'Solltemp. VL Sekundär(°C)',
+                        'Max.Rücklauftemp.primär(°C)',
+                        'B-Solltemp.(°C)',
+                        'B-Status Pumpe',
+                        'B-Status Kreis',
+                        'B-Status Mischer',
+                        'Puffer EIN Oben(°C)',
+                        'Puffer AUS Unten(°C)',
+                        'Ventilstellung(%)',
+                        'AT Mittel(°C)',
+                        'AT Langzeit(°C)',
+                        'WZ Wärmemenge(kWh)',
+                        'WZ Leistung(kW)',
+                        'WZ Duchfluss(l/h)',
+                        'WZ Rücklauftemp.(°C)',
+                        'WZ Vorlauftemp.(°C)',
+                        'WZ Spreizung(°C)',
                         'Ventilstellung Gesamt(%)']
 
-        columns_values = ["timestamp", "aussentemperatur", "primär_temperatur",
-                          "sekundär_temperatur_vl", "sekundär_temperatur_rl",
-                          "sekundär_temperatur_vl_soll", "status_pump", "status_circle",
-                          "status_mischer", "puffer_oben_ein", "puffer_unten_aus",
-                          "ventilstellung", "aussentemperatur_mittel", "aussentemperatur_langzeit",
-                          "wärmemenge_kwh", "leistung_kw", "durchfluss_liter_per_hour",
-                          "rücklauftemperatur", "vorlauftemperatur", "spreizung_celsius",
+        columns_values = ["timestamp",
+                          "aussentemperatur",
+                          "primär_temperatur",
+                          "sekundär_temperatur_vl",
+                          "sekundär_temperatur_rl",
+                          "sekundär_temperatur_vl_soll",
+                          "maximale_rücklauftemperature",
+                          "status_pump",
+                          "status_kreis",
+                          "status_mischer",
+                          "puffer_oben_ein",
+                          "puffer_unten_aus",
+                          "ventilstellung",
+                          "aussentemperatur_mittel",
+                          "aussentemperatur_langzeit",
+                          "wärmemenge_kwh",
+                          "leistung_kw",
+                          "durchfluss_liter_per_hour",
+                          "rücklauftemperatur",
+                          "vorlauftemperatur",
+                          "fehleranzeige"
+                          "spreizung_celsius",
                           "ventilstellung_gesamt"]
+
         columns_dict = dict(zip(columns_keys, columns_values))
         return df.rename(columns=columns_dict)
 
     @classmethod
     def _set_datetimeindex(cls, df):
         return df.set_index("timestamp")
+
